39587f00a2ed654629f642338f31f7a640f592fe
Daniel Camozzo <dcamo001@gmail.com>
2018-08-28 15:28:44 -0700

    Added headers to response in handler

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3();
 
-module.exports.fetchImages = async (event, context) => {
+module.exports.fetchImages = function(event, context, callback) {
   const bucket = "camozzomedia";
   const path_to_folder = "/images/";
   const params = { Bucket: bucket, Delimiter: path_to_folder };
@@ -14,12 +14,16 @@ module.exports.fetchImages = async (event, context) => {
       ? "Could not load objects from s3" + err
       : "Loaded " + data.Contents.length + " items from s3";
 
-    return {
+    callback(null, {
       statusCode: err ? 500 : 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      },
       body: JSON.stringify({
         message,
         gallery: data.Contents
       })
-    };
+    });
   });
 };
